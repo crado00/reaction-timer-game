702191d4d1c8250b267b5023101254bed7aa6d10
crado00 <alrlawowns@gmail.com>
2025-08-01 13:33:09 +0900

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from 'react'
+import ReactionTimerGame from './components/ReactionTimerGame';
 
 const App = () => {
-  return <div className="bg-red-100">App</div>;
+  const [isShow, setIsShow] = useState(false);
+
+  return (
+    <div className="bg-green-100 min-h-screen flex justify-center items-center">
+        <ReactionTimerGame/>
+    </div>
+  );
 };
 
 export default App;
